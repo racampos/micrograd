0cc9adf7d1882f62d7c81b79813640fba41e8d6a
Rafael Campos <racampos@gmail.com>
2024-05-07 14:13:01 -0600

    Simple implementation with ownership issues.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::ops::{Add, Div, Mul, Sub};
 enum Op {
     Add,
     Mul,
+    Tanh,
 }
 
 #[derive(Debug, Clone)]
@@ -14,26 +15,26 @@ struct Value {
     _prev: Option<(Box<Value>, Box<Value>)>,
     _op: Option<Op>,
     grad: f64,
-    _backward: Option<fn(Value) -> Value>,
 }
+
+const DUMMY_VALUE: Value = Value {
+    data: 0.0,
+    _prev: None,
+    _op: None,
+    grad: 0.0,
+};
 
 impl Value {
     fn new(data: f64) -> Self {
-        Self::new_ext(data, None, None, None)
+        Self::new_ext(data, None, None)
     }
 
-    fn new_ext(
-        data: f64,
-        _children: Option<(Box<Value>, Box<Value>)>,
-        _op: Option<Op>,
-        _backward: Option<fn(Value) -> Value>,
-    ) -> Self {
+    fn new_ext(data: f64, _children: Option<(Box<Value>, Box<Value>)>, _op: Option<Op>) -> Self {
         Self {
             data,
             _prev: _children,
             _op,
             grad: 0.0,
-            _backward,
         }
     }
 
@@ -42,10 +43,33 @@ impl Value {
         let t = (f64::exp(2.0 * x) - 1.0) / (f64::exp(2.0 * x) + 1.0);
         Self::new_ext(
             t,
-            Some((Box::new(self.clone()), Box::new(self.clone()))),
-            None,
-            None,
+            Some((Box::new(self), Box::new(DUMMY_VALUE))),
+            Some(Op::Tanh),
         )
+    }
+
+    fn _backward(&mut self) {
+        if let Some(ref mut _prev) = self._prev {
+            let (ref mut a, ref mut b) = *_prev;
+            println!("{:?}", self._op);
+            match self._op {
+                Some(Op::Add) => {
+                    a.grad += self.grad;
+                    b.grad += self.grad;
+                    println!("a.grad: {}, b.grad: {}", a.grad, b.grad);
+                }
+                Some(Op::Mul) => {
+                    a.grad += b.data * self.grad;
+                    b.grad += a.data * self.grad;
+                }
+                Some(Op::Tanh) => {
+                    let t = (f64::exp(2.0 * a.data) - 1.0) / (f64::exp(2.0 * a.data) + 1.0);
+                    a.grad = (1.0 - t.powf(2.0)) * self.grad;
+                    println!("a.grad: {}", a.grad);
+                }
+                None => {}
+            }
+        }
     }
 }
 
@@ -67,7 +91,6 @@ impl Add<Value> for Value {
             self.data + other.data,
             Some((Box::new(self), Box::new(other))),
             Some(Op::Add),
-            Some(),
         )
     }
 }
@@ -107,86 +130,29 @@ impl Div<Value> for Value {
         )
     }
 }
-
-// let mut a = Value::new(2.0);
-// let mut b = Value::new(-3.0);
-// let mut c = Value::new(10.0);
-// let mut e = a.clone() * b.clone();
-// let mut d = e.clone() + c.clone();
-// let mut f = Value::new(-2.0);
-// let mut L = d.clone()*f.clone();
-
-// L.grad = 1.0;
-// f.grad = 4.0;
-// d.grad = -2.0;
-// e.grad = -2.0;
-// c.grad = -2.0;
-// a.grad = 6.0;
-// b.grad = -4.0;
-
-// a.data += 0.01 * a.grad;
-// b.data += 0.01 * b.grad;
-// c.data += 0.01 * c.grad;
-// f.data += 0.01 * f.grad;
-
-// e = a * b;
-// d = e + c;
-// L = d * f;
-
-// println!("{}", L.data);
-
-// *******
-
-// let h = 0.0001;
-
-// let a = Value::new(2.0);
-// let b = Value::new(-3.0);
-// let c = Value::new(10.0);
-// let e = a*b;
-// let d = e + c;
-// let f = Value::new(-2.0);
-// let L = d*f;
-// let L1 = L.data;
-
-// let a = Value::new(2.0 + h);
-// let b = Value::new(-3.0);
-// let c = Value::new(10.0);
-// let e = a*b;
-// let d = e + c;
-// let f = Value::new(-2.0);
-// let L = d*f;
-// let L2 = L.data;
-
-// println!("{}", ((L2 - L1)/h));
 fn main() {
-    let mut x1 = Value::new(2.0);
-    let mut x2 = Value::new(0.0);
+    let x1 = Value::new(2.0);
+    let x2 = Value::new(0.0);
 
-    let mut w1 = Value::new(-3.0);
-    let mut w2 = Value::new(1.0);
+    let w1 = Value::new(-3.0);
+    let w2 = Value::new(1.0);
 
-    let mut b = Value::new(6.8813735870195432);
+    let b = Value::new(6.8813735870195432);
 
-    let mut x1w1 = x1.clone() * w1.clone();
-    let mut x2w2 = x2.clone() * w2.clone();
+    let x1w1 = x1 * w1;
+    let x2w2 = x2 * w2;
+    println!("x1w1: {}, x2w2: {}", x1w1, x2w2);
 
-    let mut x1w1x2w2 = x1w1.clone() + x2w2.clone();
-    let mut n = x1w1x2w2.clone() + b.clone();
+    let x1w1x2w2 = x1w1 + x2w2;
+    println!("x1w1x2w2: {}", x1w1x2w2);
 
-    let mut o = n.clone().tanh();
+    let mut n = x1w1x2w2 + b;
+    println!("n: {}", n);
 
-    x1.grad = w1.data * x1w1.grad;
-    w1.grad = x1.data * x1w1.grad;
-    x2.grad = w2.data * x2w2.grad;
-    w2.grad = x2.data * x2w2.grad;
+    let mut o = n.tanh();
+    println!("o: {}", o);
 
-    x1w1.grad = 0.5;
-    x2w2.grad = 0.5;
-
-    x1w1x2w2.grad = 0.5;
-    b.grad = 0.5;
-    n.grad = 0.5;
     o.grad = 1.0;
-
-    println!("{}", 1.0 - o.data.powf(2.0));
+    o._backward();
+    n._backward();
 }
